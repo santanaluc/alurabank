bc93839762a7a75d0e621bacd2efb2ace614bf10
Lucas Santana <lucas.s.s.silva@hotmail.com>
2021-05-03 15:54:36 -0300

    modificando parâmetros opcionais

--- alurabank/app/ts/views/View.ts
+++ alurabank/app/ts/views/View.ts
@@ -1,13 +1,23 @@
 export abstract class View<T> {
 
     protected _elemento: JQuery;
-
-    constructor(seletor: string) {
+    private _escapar: boolean;
+    // o parâmetro opcional tem que ser o ultimo do constructor
+    constructor(seletor: string, escapar?:boolean) {
 
         this._elemento = $(seletor);
+        this._escapar = escapar;
     }
 
     update(model: T) {
+
+        let template = this.template(model);
+
+        if (this._escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script>/g, '');
+        }
+        
+        this._elemento.html(this.template(model));
 
         this._elemento.html(this.template(model));
     }
