37ec5492cf062a71280f5d7a0c7646b76d80eaa8
Lucas Santana <lucas.s.s.silva@hotmail.com>
2021-05-03 15:43:27 -0300

    utilizando readonly

--- alurabank/app/ts/models/negociacao.ts
+++ alurabank/app/ts/models/negociacao.ts
@@ -1,22 +1,11 @@
 export class Negociacao {
 
-    constructor(private _data:Date, private _quantidade:number, private _valor:number) {} //depois de criada nao pode ser alterada, por isso o underline.
+    constructor(readonly data:Date, readonly quantidade:number, readonly valor:number) {} //depois de criada nao pode ser alterada, por isso o underline.
 
     //get serve para mostrar os dados.
-    get data() {
-        return this._data;
-    }
-
-    get quantidade() {
-        return this._quantidade;
-    }
-
-    get valor() {
-        return this._valor;
-    }
 
     //retorna o valor total de negociacoes
     get volume() {
-        return this._quantidade * this._valor
+        return this.quantidade * this.valor
     }
 }
