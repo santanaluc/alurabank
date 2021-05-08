ba1ebad5e6ec85d47ed94c3b935eb4389e2bede0
Lucas Santana <lucas.s.s.silva@hotmail.com>
2021-05-07 21:05:32 -0300

    add decorators

--- alurabank/app/ts/controllers/NegociacaoController.ts
+++ alurabank/app/ts/controllers/NegociacaoController.ts
@@ -1,5 +1,7 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacoes, Negociacao } from '../models/index';
+import { logarTempoDeExecucao } from "../helpers/decorators/index";
+
 
 export class NegociacaoController {
 
@@ -17,6 +19,7 @@ export class NegociacaoController {
         this._negociacoesView.update(this._negociacoes);
     }
 
+    @logarTempoDeExecucao()
     adiciona(event: Event) {
 
         event.preventDefault();
