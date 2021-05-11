3771ba627d5a0c5717445a13334d30302e6f0a17
Lucas Santana <lucas.s.s.silva@hotmail.com>
2021-05-11 16:19:53 -0300

    definindo uma interface para a api

---
+++ alurabank/app/ts/models/NegociacaoParcial.ts
@@ -0,0 +1,7 @@
+export interface NegociacaoParcial {
+
+    vezes: number;
+    montante: number;
+}
+
+//vantagem dessa interface é poder acessar uma api, definir tipos e identificar erros em runtime me alertando onde tem que alterar.
